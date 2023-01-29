040b3ecb9b49132eb456eef5e2f58283040ec069
priyabrata <priyabratapinku7@gmail.com>
2023-01-29 11:57:37 +0530

    Day - 02 practice

---
+++ Day - 3/1_num_even_odd.py
@@ -0,0 +1,8 @@
+number = int(input("Which number do you want to check? "))
+
+num_check = number % 2
+
+if num_check == 0:
+    print("This is an even number.")
+else:
+    print("This is an odd number.")
---
+++ Day - 3/4_leap_year.py
@@ -0,0 +1,15 @@
+year = int(input("Which year do you want to check? "))
+
+# on every year that is evenly divisible by 4 
+if year % 4 == 0:
+# except** every year that is evenly divisible by 100 
+    if year % 100 == 0:
+# unless** the year is also evenly divisible by 400
+        if year % 400 == 0:
+            print("Leap year.")
+        else:
+            print("Not leap year.")       
+    else:
+        print("Leap year.")
+else:
+    print("Not leap year.")
